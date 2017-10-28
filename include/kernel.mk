iyunlink/openwrt-21.02-for-mt7987

commit 475659dea514f7f0ea8d4078b1a522f8b9e0e8ac

build: ensure PKG_INFO_DIR exists before trying to use it
PKG_INFO_DIR is only created at the finish step of the first package
build, but kernel modules now use it at the start. Doing a parallel build
could cause a kernel module to be the first package, breaking the build.

Fix this by ensuring the directory exists.

Fixes: f56a8d225d6b ("kernel: collect module symvers for external modules")
Signed-off-by: Jonas Gorski <jonas.gorski@gmail.com>

include/kernel.mk
@@ -133,6 +133,7 @@ define populate_module_symvers
 endef
 
 define collect_module_symvers
+	@mkdir -p $(PKG_INFO_DIR)
 	for subdir in $(PKG_EXTMOD_SUBDIRS); do \
 		grep -F $(PKG_BUILD_DIR) $(PKG_BUILD_DIR)/$$$$subdir/Module.symvers >> $(PKG_BUILD_DIR)/Module.symvers.tmp; \
 	done; \

commit 8e785046fdd338c0e4a9ff5588bcf8341cf7d656

build: actually fix the creation of PKG_INFO_DIR
The creation was accidentally moved to the wrong hook, fix it by moving
it to the pre hook.

Fixes: 475659dea514 ("build: ensure PKG_INFO_DIR exists before trying to use it")
Signed-off-by: Jonas Gorski <jonas.gorski@gmail.com>

include/kernel.mk
@@ -126,6 +126,7 @@ endif
 PKG_EXTMOD_SUBDIRS ?= .
 
 define populate_module_symvers
+	@mkdir -p $(PKG_INFO_DIR)
 	cat /dev/null > $(PKG_INFO_DIR)/$(PKG_NAME).symvers; \
 	for subdir in $(PKG_EXTMOD_SUBDIRS); do \
 		cat $(PKG_INFO_DIR)/*.symvers 2>/dev/null > $(PKG_BUILD_DIR)/$$$$subdir/Module.symvers; \
@@ -133,7 +134,6 @@ define populate_module_symvers
 endef
 
 define collect_module_symvers
-	@mkdir -p $(PKG_INFO_DIR)
 	for subdir in $(PKG_EXTMOD_SUBDIRS); do \
 		grep -F $(PKG_BUILD_DIR) $(PKG_BUILD_DIR)/$$$$subdir/Module.symvers >> $(PKG_BUILD_DIR)/Module.symvers.tmp; \
 	done; \