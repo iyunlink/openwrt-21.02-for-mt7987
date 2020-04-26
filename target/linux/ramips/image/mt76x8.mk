iyunlink/openwrt-21.02-for-mt7987

commit 978951f3e8927828cb1ee1335290e19564516934

ramips/mt76x8: Synchronize Makefiles with DTS compatible
Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>

target/linux/ramips/image/mt76x8.mk
@@ -52,23 +52,25 @@ define Device/tama_w06
 endef
 TARGET_DEVICES += tama_w06
 
-define Device/duzun-dm06
+define Device/duzun_dm06
   DTS := DUZUN-DM06
   DEVICE_VENDOR := DuZun
   DEVICE_MODEL := DM06
   DEVICE_PACKAGES := kmod-usb2 kmod-usb-ohci kmod-usb-ledtrig-usbport
+  SUPPORTED_DEVICES += duzun-dm06
 endef
-TARGET_DEVICES += duzun-dm06
+TARGET_DEVICES += duzun_dm06
 
-define Device/gl-mt300n-v2
+define Device/glinet_gl-mt300n-v2
   DTS := GL-MT300N-V2
   IMAGE_SIZE := 16064k
   DEVICE_VENDOR := GL.iNet
   DEVICE_MODEL := GL-MT300N
   DEVICE_VARIANT := V2
   DEVICE_PACKAGES := kmod-usb2 kmod-usb-ohci
+  SUPPORTED_DEVICES += gl-mt300n-v2
 endef
-TARGET_DEVICES += gl-mt300n-v2
+TARGET_DEVICES += glinet_gl-mt300n-v2
 
 define Device/glinet_vixmini
   DTS := VIXMINI
@@ -79,19 +81,20 @@ define Device/glinet_vixmini
 endef
 TARGET_DEVICES += glinet_vixmini
 
-define Device/hc5661a
+define Device/hiwifi_hc5661a
   DTS := HC5661A
   IMAGE_SIZE := $(ralink_default_fw_size_16M)
   DEVICE_VENDOR := HiWiFi
   DEVICE_MODEL := HC5661A
+  SUPPORTED_DEVICES += hc5661a
 endef
-TARGET_DEVICES += hc5661a
+TARGET_DEVICES += hiwifi_hc5661a
 
 define Device/hilink_hlk-7628n
   DTS := HLK-7628N
   IMAGE_SIZE := $(ralink_default_fw_size_32M)
   DEVICE_VENDOR := HILINK
-  DEVICE_MODEL := HLK7628N
+  DEVICE_MODEL := HLK-7628N
 endef
 TARGET_DEVICES += hilink_hlk-7628n
 
@@ -104,44 +107,47 @@ define Device/hiwifi_hc5861b
 endef
 TARGET_DEVICES += hiwifi_hc5861b
 
-define Device/LinkIt7688
+define Device/mediatek_linkit-smart-7688
   DTS := LINKIT7688
   IMAGE_SIZE := $(ralink_default_fw_size_32M)
-  SUPPORTED_DEVICES := linkits7688 linkits7688d
   DEVICE_VENDOR := MediaTek
   DEVICE_MODEL := LinkIt Smart 7688
   DEVICE_PACKAGES:= kmod-usb2 kmod-usb-ohci uboot-envtools
+  SUPPORTED_DEVICES += linkits7688 linkits7688d
 endef
-TARGET_DEVICES += LinkIt7688
+TARGET_DEVICES += mediatek_linkit-smart-7688
 
-define Device/mac1200r-v2
+define Device/mercury_mac1200r-v2
   DTS := MAC1200RV2
   DEVICE_VENDOR := Mercury
   DEVICE_MODEL := MAC1200R
   DEVICE_VARIANT := v2.0
   SUPPORTED_DEVICES := mac1200rv2
   DEVICE_PACKAGES := kmod-mt76x2
+  SUPPORTED_DEVICES += mac1200rv2
 endef
-TARGET_DEVICES += mac1200r-v2
+TARGET_DEVICES += mercury_mac1200r-v2
 
-define Device/miwifi-nano
+define Device/xiaomi_miwifi-nano
   DTS := MIWIFI-NANO
   IMAGE_SIZE := $(ralink_default_fw_size_16M)
   DEVICE_VENDOR := Xiaomi
   DEVICE_MODEL := MiWiFi Nano
   DEVICE_PACKAGES := kmod-usb2 kmod-usb-ohci kmod-usb-ledtrig-usbport
+  SUPPORTED_DEVICES += miwifi-nano
 endef
-TARGET_DEVICES += miwifi-nano
+TARGET_DEVICES += xiaomi_miwifi-nano
 
-define Device/mt7628
+define Device/mediatek_mt7628an-eval-board
   DTS := MT7628
   BLOCKSIZE := 64k
   IMAGE_SIZE := $(ralink_default_fw_size_4M)
   DEVICE_VENDOR := MediaTek
   DEVICE_MODEL := MT7628 EVB
   DEVICE_PACKAGES := kmod-usb2 kmod-usb-ohci kmod-usb-ledtrig-usbport
+  SUPPORTED_DEVICES += mt7628
 endef
-TARGET_DEVICES += mt7628
+TARGET_DEVICES += mediatek_mt7628an-eval-board
 
 define Device/netgear_r6120
   DTS := R6120
@@ -161,32 +167,35 @@ define Device/netgear_r6120
 endef
 TARGET_DEVICES += netgear_r6120
 
-define Device/omega2
+define Device/onion_omega2
   DTS := OMEGA2
   IMAGE_SIZE := $(ralink_default_fw_size_16M)
   DEVICE_VENDOR := Onion
   DEVICE_MODEL := Omega2
   DEVICE_PACKAGES:= kmod-usb2 kmod-usb-ohci uboot-envtools
+  SUPPORTED_DEVICES += omega2
 endef
-TARGET_DEVICES += omega2
+TARGET_DEVICES += onion_omega2
 
-define Device/omega2p
+define Device/onion_omega2p
   DTS := OMEGA2P
   IMAGE_SIZE := $(ralink_default_fw_size_32M)
   DEVICE_VENDOR := Onion
   DEVICE_MODEL := Omega2+
   DEVICE_PACKAGES:= kmod-usb2 kmod-usb-ohci uboot-envtools kmod-sdhci-mt7620
+  SUPPORTED_DEVICES += omega2p
 endef
-TARGET_DEVICES += omega2p
+TARGET_DEVICES += onion_omega2p
 
-define Device/pbr-d1
+define Device/d-team_pbr-d1
   DTS := PBR-D1
   IMAGE_SIZE := $(ralink_default_fw_size_16M)
   DEVICE_VENDOR := PandoraBox
   DEVICE_MODEL := PBR-D1
   DEVICE_PACKAGES := kmod-usb2 kmod-usb-ohci
+  SUPPORTED_DEVICES += pbr-d1
 endef
-TARGET_DEVICES += pbr-d1
+TARGET_DEVICES += d-team_pbr-d1
 
 define Device/rakwireless_rak633
   DTS := RAK633
@@ -242,7 +251,7 @@ define Device/tplink_tl-wr802n-v4
 endef
 TARGET_DEVICES += tplink_tl-wr802n-v4
 
-define Device/tl-wr840n-v4
+define Device/tplink_tl-wr840n-v4
   $(Device/tplink)
   DTS := TL-WR840NV4
   IMAGE_SIZE := 7808k
@@ -253,10 +262,11 @@ define Device/tl-wr840n-v4
   TPLINK_HWREV := 0x1
   TPLINK_HWREVADD := 0x4
   TPLINK_HVERSION := 3
+  SUPPORTED_DEVICES += tl-wr840n-v4
 endef
-TARGET_DEVICES += tl-wr840n-v4
+TARGET_DEVICES += tplink_tl-wr840n-v4
 
-define Device/tl-wr840n-v5
+define Device/tplink_tl-wr840n-v5
   DTS := TL-WR840NV5
   IMAGE_SIZE := 3904k
   DEVICE_MODEL := TL-WR840N
@@ -270,10 +280,11 @@ define Device/tl-wr840n-v5
   KERNEL_INITRAMFS := $(KERNEL_DTB) | tplink-v2-header -e
   IMAGE/sysupgrade.bin := tplink-v2-image -s -e | append-metadata | \
 	check-size $$$$(IMAGE_SIZE)
+  SUPPORTED_DEVICES += tl-wr840n-v5
 endef
-TARGET_DEVICES += tl-wr840n-v5
+TARGET_DEVICES += tplink_tl-wr840n-v5
 
-define Device/tl-wr841n-v13
+define Device/tplink_tl-wr841n-v13
   $(Device/tplink)
   DTS := TL-WR841NV13
   IMAGE_SIZE := 7808k
@@ -284,8 +295,9 @@ define Device/tl-wr841n-v13
   TPLINK_HWREV := 0x268
   TPLINK_HWREVADD := 0x13
   TPLINK_HVERSION := 3
+  SUPPORTED_DEVICES += tl-wr841n-v13
 endef
-TARGET_DEVICES += tl-wr841n-v13
+TARGET_DEVICES += tplink_tl-wr841n-v13
 
 define Device/tplink_tl-wr841n-v14
   $(Device/tplink)
@@ -411,35 +423,38 @@ define Device/tplink_tl-wr902ac-v3
 endef
 TARGET_DEVICES += tplink_tl-wr902ac-v3
 
-define Device/u7628-01-128M-16M
+define Device/unielec_u7628-01-128m-16m
   DTS := U7628-01-128M-16M
   IMAGE_SIZE := 16064k
   DEVICE_VENDOR := UniElec
   DEVICE_MODEL := U7628-01
   DEVICE_VARIANT := 128M RAM/16M flash
   DEVICE_PACKAGES := kmod-usb2 kmod-usb-ohci kmod-usb-ledtrig-usbport
+  SUPPORTED_DEVICES += u7628-01-128M-16M
 endef
-TARGET_DEVICES += u7628-01-128M-16M
+TARGET_DEVICES += unielec_u7628-01-128m-16m
 
-define Device/vocore2
+define Device/vocore_vocore2
   DTS := VOCORE2
   IMAGE_SIZE := $(ralink_default_fw_size_16M)
   DEVICE_VENDOR := VoCore
   DEVICE_MODEL := VoCore2
   DEVICE_PACKAGES := kmod-usb2 kmod-usb-ohci kmod-usb-ledtrig-usbport \
     kmod-sdhci-mt7620
+  SUPPORTED_DEVICES += vocore2
 endef
-TARGET_DEVICES += vocore2
+TARGET_DEVICES += vocore_vocore2
 
-define Device/vocore2lite
+define Device/vocore_vocore2-lite
   DTS := VOCORE2LITE
   IMAGE_SIZE := $(ralink_default_fw_size_16M)
   DEVICE_VENDOR := VoCore
   DEVICE_MODEL := VoCore2-Lite
   DEVICE_PACKAGES := kmod-usb2 kmod-usb-ohci kmod-usb-ledtrig-usbport \
     kmod-sdhci-mt7620
+  SUPPORTED_DEVICES += vocore2lite
 endef
-TARGET_DEVICES += vocore2lite
+TARGET_DEVICES += vocore_vocore2-lite
 
 define Device/wavlink_wl-wn570ha1
   DTS := WL-WN570HA1
@@ -460,7 +475,7 @@ define Device/wavlink_wl-wn575a3
 endef
 TARGET_DEVICES += wavlink_wl-wn575a3
 
-define Device/wcr-1166ds
+define Device/buffalo_wcr-1166ds
   DTS := WCR-1166DS
   BUFFALO_TAG_PLATFORM := MTK
   BUFFALO_TAG_VERSION := 9.99
@@ -475,8 +490,9 @@ define Device/wcr-1166ds
   DEVICE_VENDOR := Buffalo
   DEVICE_MODEL := WCR-1166DS
   DEVICE_PACKAGES := kmod-mt76x2
+  SUPPORTED_DEVICES += wcr-1166ds
 endef
-TARGET_DEVICES += wcr-1166ds
+TARGET_DEVICES += buffalo_wcr-1166ds
 
 define Device/widora_neo-16m
   DTS := WIDORA-NEO-16M
@@ -507,23 +523,25 @@ define Device/wiznet_wizfi630s
 endef
 TARGET_DEVICES += wiznet_wizfi630s
 
-define Device/wrtnode2p
+define Device/wrtnode_wrtnode2p
   DTS := WRTNODE2P
   IMAGE_SIZE := $(ralink_default_fw_size_16M)
   DEVICE_VENDOR := WRTnode
   DEVICE_MODEL := WRTnode 2P
   DEVICE_PACKAGES := kmod-usb2 kmod-usb-ohci kmod-usb-ledtrig-usbport
+  SUPPORTED_DEVICES += wrtnode2p
 endef
-TARGET_DEVICES += wrtnode2p
+TARGET_DEVICES += wrtnode_wrtnode2p
 
-define Device/wrtnode2r
+define Device/wrtnode_wrtnode2r
   DTS := WRTNODE2R
   IMAGE_SIZE := $(ralink_default_fw_size_16M)
   DEVICE_VENDOR := WRTnode
   DEVICE_MODEL := WRTnode 2R
   DEVICE_PACKAGES := kmod-usb2 kmod-usb-ohci
+  SUPPORTED_DEVICES += wrtnode2r
 endef
-TARGET_DEVICES += wrtnode2r
+TARGET_DEVICES += wrtnode_wrtnode2r
 
 define Device/xiaomi_mir4a-100m
   DTS := XIAOMI-MIR4A-100M

target/linux/ramips/base-files/etc/board.d/01_leds
@@ -192,12 +192,12 @@ glinet,gl-mt300n|\
 glinet,gl-mt750)
 	set_wifi_led "$boardname:wlan"
 	;;
-gl-mt300n-v2)
+glinet,gl-mt300n-v2)
 	set_wifi_led "$boardname:red:wlan"
 	ucidef_set_led_switch "wan" "wan" "$boardname:green:wan" "switch0" "0x1"
 	;;
 hiwifi,hc5661|\
-hc5661a)
+hiwifi,hc5661a)
 	ucidef_set_led_netdev "internet" "internet" "$boardname:blue:internet" "eth0.2"
 	set_wifi_led "$boardname:blue:wlan2g"
 	;;
@@ -232,7 +232,7 @@ lava,lr-25g001)
 	ucidef_set_led_netdev "wlan2g" "WiFi 2.4GHz" "$boardname:green:wlan2g" "wlan1"
 	ucidef_set_led_netdev "wlan5g" "WiFi 5GHz" "$boardname:green:wlan5g" "wlan0"
 	;;
-linkits7688)
+mediatek,linkit-smart-7688)
 	ucidef_set_led_wlan "wifi" "wifi" "linkit-smart-7688:orange:wifi" "phy0tpt"
 	;;
 m2m)
@@ -313,12 +313,12 @@ tama,w06)
 	ucidef_set_led_netdev "wan" "WAN" "$boardname:green:wan" "eth0"
 	ucidef_set_led_wlan "wlan" "WLAN" "$boardname:green:wlan" "phy0tpt"
 	;;
-tl-wr840n-v4)
+tplink,tl-wr840n-v4)
 	ucidef_set_led_wlan "wlan2g" "wlan2g" "$boardname:green:wlan" "phy0tpt"
 	ucidef_set_led_switch "lan" "lan" "$boardname:green:lan" "switch0" "0x1e"
 	ucidef_set_led_switch "wan" "wan" "$boardname:green:wan" "switch0" "0x01"
 	;;
-tl-wr841n-v13)
+tplink,tl-wr841n-v13)
 	ucidef_set_led_wlan "wlan2g" "wlan2g" "$boardname:green:wlan" "phy0tpt"
 	ucidef_set_led_switch "lan1" "lan1" "$boardname:green:lan1" "switch0" "0x2"
 	ucidef_set_led_switch "lan2" "lan2" "$boardname:green:lan2" "switch0" "0x4"
@@ -374,7 +374,7 @@ kimax,u25awf-h1)
 	set_wifi_led "u25awf:red:wifi"
 	ucidef_set_led_netdev "eth" "eth" "u25awf:green:lan" "eth0"
 	;;
-u7628-01-128M-16M)
+unielec,u7628-01-128m-16m)
 	ucidef_set_led_switch "lan1" "lan1" "u7628-01:green:lan1" "switch0" "0x2"
 	ucidef_set_led_switch "lan2" "lan2" "u7628-01:green:lan2" "switch0" "0x4"
 	ucidef_set_led_switch "lan3" "lan3" "u7628-01:green:lan3" "switch0" "0x8"

target/linux/ramips/base-files/etc/board.d/02_network
@@ -57,14 +57,14 @@ ramips_setup_interfaces()
 	edimax,ew-7478ac|\
 	ht-tm02|\
 	kimax,u35wf|\
-	linkits7688 | \
+	mediatek,linkit-smart-7688|\
 	m2m|\
 	microduino,microwrt|\
 	mikrotik,rbm11g|\
 	mpr-a2|\
 	ncs601w|\
-	omega2 | \
-	omega2p | \
+	onion,omega2|\
+	onion,omega2p|\
 	thunder,timecloud|\
 	tplink,tl-wa801nd-v5|\
 	w150m|\
@@ -99,17 +99,17 @@ ramips_setup_interfaces()
 	dlink,dwr-922-e2|\
 	afoundry,ew1200|\
 	firefly,firewrt|\
-	hc5661a|\
+	hiwifi,hc5661a|\
 	hiwifi,hc5962|\
 	hlk-rm04|\
 	phicomm,k2p|\
 	kn|\
 	zyxel,keenetic-omni|\
-	mac1200rv2|\
+	mercury,mac1200r-v2|\
 	xiaomi,miwifi-mini|\
-	miwifi-nano|\
+	xiaomi,miwifi-nano|\
 	mediatek,mt7621-eval-board|\
-	mt7628|\
+	mediatek,mt7628an-eval-board|\
 	mtc,wr1201|\
 	planex,mzk-750dhp|\
 	mzk-w300nh2|\
@@ -168,7 +168,7 @@ ramips_setup_interfaces()
 	planex,mzk-ex300np|\
 	planex,mzk-ex750np|\
 	sercomm,na930|\
-	pbr-d1|\
+	d-team,pbr-d1|\
 	ravpower,wd03|\
 	tama,w06|\
 	tplink,tl-mr3020-v3|\
@@ -243,17 +243,17 @@ ramips_setup_interfaces()
 	tplink,archer-c50-v4|\
 	tplink,tl-mr3420-v5|\
 	tplink,tl-wr842n-v5|\
-	tl-wr840n-v4|\
-	tl-wr840n-v5|\
-	tl-wr841n-v13|\
+	tplink,tl-wr840n-v4|\
+	tplink,tl-wr840n-v5|\
+	tplink,tl-wr841n-v13|\
 	tplink,tl-wr841n-v14|\
-	u7628-01-128M-16M|\
+	unielec,u7628-01-128m-16m|\
 	ubiquiti,edgerouterx|\
 	ubiquiti,edgerouterx-sfp|\
 	ur-326n4g|\
 	wrtnode,wrtnode|\
-	wrtnode2p | \
-	wrtnode2r | \
+	wrtnode,wrtnode2p|\
+	wrtnode,wrtnode2r|\
 	youhua,wr1200js|\
 	zbtlink,zbt-wa05|\
 	zyxel,keenetic-extra-ii)
@@ -280,7 +280,7 @@ ramips_setup_interfaces()
 		ucidef_add_switch "switch0" \
 			"0:lan" "4:lan" "6@eth0"
 		;;
-	gl-mt300n-v2)
+	glinet,gl-mt300n-v2)
 		ucidef_add_switch "switch0" \
 			"1:lan" "0:wan" "6@eth0"
 		;;
@@ -314,7 +314,7 @@ ramips_setup_interfaces()
 		ucidef_add_switch "switch0" \
 			"0:lan" "1:lan" "2:lan" "3:lan" "4:wan" "9@eth0"
 		;;
-	duzun-dm06)
+	duzun,dm06)
 		ucidef_add_switch "switch0" \
 			"1:lan" "0:wan" "6@eth0"
 		;;
@@ -419,8 +419,8 @@ ramips_setup_interfaces()
 		ucidef_add_switch "switch0" \
 			"0:lan" "4:wan" "6@eth0"
 		;;
-	vocore2|\
-	vocore2lite)
+	vocore,vocore2|\
+	vocore,vocore2-lite)
 		ucidef_add_switch "switch0" \
 			"0:lan" "2:lan" "6t@eth0"
 		;;
@@ -432,7 +432,7 @@ ramips_setup_interfaces()
 		ucidef_add_switch "switch0" \
 			"1:lan" "2:lan" "3:lan" "4:lan" "0:wan" "5@eth0"
 		;;
-	wcr-1166ds)
+	buffalo,wcr-1166ds)
 		ucidef_add_switch "switch0" \
 			"3:lan" "4:wan" "6@eth0"
 		;;
@@ -570,14 +570,14 @@ ramips_setup_macs()
 	samknows,whitebox-v8)
 		wan_mac=$(mtd_get_mac_binary factory 57350)
 		;;
-	gl-mt300n-v2|\
+	glinet,gl-mt300n-v2|\
 	whr-g300n)
 		wan_mac=$(mtd_get_mac_binary factory 4)
 		;;
 	hiwifi,hc5661|\
 	hiwifi,hc5761|\
 	hiwifi,hc5861|\
-	hc5661a|\
+	hiwifi,hc5661a|\
 	hiwifi,hc5962|\
 	hiwifi,hc5861b)
 		lan_mac=`mtd_get_mac_ascii bdinfo "Vfac_mac "`
@@ -597,11 +597,11 @@ ramips_setup_macs()
 	zyxel,keenetic-viva)
 		wan_mac=$(mtd_get_mac_binary factory 40)
 		;;
-	linkits7688)
+	mediatek,linkit-smart-7688)
 		wan_mac=$(mtd_get_mac_binary factory 4)
 		lan_mac=$(mtd_get_mac_binary factory 46)
 		;;
-	mac1200rv2)
+	mercury,mac1200r-v2)
 		lan_mac=$(mtd_get_mac_binary factory_info 13)
 		wan_mac=$(macaddr_add "$lan_mac" 1)
 		;;
@@ -621,8 +621,8 @@ ramips_setup_macs()
 		lan_mac=$(cat /sys/class/net/eth0/address)
 		lan_mac=$(macaddr_add "$lan_mac" 2)
 		;;
-	omega2|\
-	omega2p)
+	onion,omega2|\
+	onion,omega2p)
 		wan_mac=$(mtd_get_mac_binary factory 4)
 		lan_mac=$(mtd_get_mac_binary factory 46)
 		;;
@@ -664,7 +664,7 @@ ramips_setup_macs()
 		lan_mac=$(cat /sys/class/net/eth0/address)
 		wan_mac=$(macaddr_add "$lan_mac" 5)
 		;;
-	wcr-1166ds|\
+	buffalo,wcr-1166ds|\
 	buffalo,wsr-1166dhp)
 		local index="$(find_mtd_index "board_data")"
 		wan_mac="$(grep -m1 mac= "/dev/mtd${index}" | cut -d= -f2)"

target/linux/ramips/base-files/lib/ramips.sh
@@ -121,9 +121,6 @@ ramips_board_detect() {
 	*"DIR-645")
 		name="dir-645"
 		;;
-	*"DuZun DM06")
-		name="duzun-dm06"
-		;;
 	*"DWR-512 B")
 		name="dwr-512-b"
 		;;
@@ -144,12 +141,6 @@ ramips_board_detect() {
 		;;
 	*"FreeStation5")
 		name="freestation5"
-		;;
-	*"GL-MT300N-V2")
-		name="gl-mt300n-v2"
-		;;
-	*"HC5661A")
-		name="hc5661a"
 		;;
 	*"HG255D")
 		name="hg255d"
@@ -187,23 +178,14 @@ ramips_board_detect() {
 	*"M4 (8M)")
 		name="m4-8M"
 		;;
-	*"MediaTek LinkIt Smart 7688")
-		name="linkits7688"
-		;;
 	*"Memory 2 Move")
 		name="m2m"
-		;;
-	*"Mercury MAC1200R v2")
-		name="mac1200rv2"
 		;;
 	*"MiniEMBPlug")
 		name="miniembplug"
 		;;
 	*"MiniEMBWiFi")
 		name="miniembwifi"
-		;;
-	*"MiWiFi Nano")
-		name="miwifi-nano"
 		;;
 	*"MOFI3500-3GN")
 		name="mofi3500-3gn"
@@ -216,9 +198,6 @@ ramips_board_detect() {
 		;;
 	*"MR-102N")
 		name="mr-102n"
-		;;
-	*"MT7628AN evaluation"*)
-		name="mt7628"
 		;;
 	*"MT7688 evaluation"*)
 		name="mt7688"
@@ -249,15 +228,6 @@ ramips_board_detect() {
 		;;
 	*"NW718")
 		name="nw718"
-		;;
-	*"Onion Omega2")
-		name="omega2"
-		;;
-	*"Onion Omega2+")
-		name="omega2p"
-		;;
-	*"PBR-D1")
-		name="pbr-d1"
 		;;
 	*"PSR-680W"*)
 		name="psr-680w"
@@ -313,18 +283,6 @@ ramips_board_detect() {
 	*"TEW-714TRU")
 		name="tew-714tru"
 		;;
-	*"TL-WR840N v4")
-		name="tl-wr840n-v4"
-		;;
-	*"TL-WR840N v5")
-		name="tl-wr840n-v5"
-		;;
-	*"TL-WR841N v13")
-		name="tl-wr841n-v13"
-		;;
-	*"U7628-01 (128M RAM/16M flash)")
-		name="u7628-01-128M-16M"
-		;;
 	*"UR-326N4G")
 		name="ur-326n4g"
 		;;
@@ -343,12 +301,6 @@ ramips_board_detect() {
 	*"VoCore (16M)")
 		name="vocore-16M"
 		;;
-	*"VoCore2")
-		name="vocore2"
-		;;
-	*"VoCore2-Lite")
-		name="vocore2lite"
-		;;
 	*"W150M")
 		name="w150m"
 		;;
@@ -357,9 +309,6 @@ ramips_board_detect() {
 		;;
 	*"W502U")
 		name="w502u"
-		;;
-	*"WCR-1166DS")
-		name="wcr-1166ds"
 		;;
 	*"WCR-150GN")
 		name="wcr-150gn"
@@ -402,12 +351,6 @@ ramips_board_detect() {
 		;;
 	*"WR6202")
 		name="wr6202"
-		;;
-	*"WRTnode2R")
-		name="wrtnode2r"
-		;;
-	*"WRTnode2P")
-		name="wrtnode2p"
 		;;
 	*"WT1520 (4M)")
 		name="wt1520-4M"

target/linux/ramips/dts/VOCORE2LITE.dts
@@ -5,7 +5,7 @@
 #include <dt-bindings/gpio/gpio.h>
 
 / {
-	compatible = "vocore,vocore2lite", "vocore,vocore2", "mediatek,mt7628an-soc";
+	compatible = "vocore,vocore2-lite", "vocore,vocore2", "mediatek,mt7628an-soc";
 	model = "VoCore2-Lite";
 
 	aliases {

commit 96aabbd9b510bfd0dc97cec58976ac5d05f4ab0d

ramips: fix SUPPORTED_DEVICES for Mercury MAC1200R v2
Currently SUPPORTED_DEVICES only contains the old device string. Fix it by
removing the first assignment.

Fixes: 978951f3e892 ("ramips/mt76x8: Synchronize Makefiles with DTS compatible")
Signed-off-by: Sungbo Eo <mans0n@gorani.run>

target/linux/ramips/image/mt76x8.mk
@@ -161,7 +161,6 @@ define Device/mercury_mac1200r-v2
   DEVICE_VENDOR := Mercury
   DEVICE_MODEL := MAC1200R
   DEVICE_VARIANT := v2.0
-  SUPPORTED_DEVICES := mac1200rv2
   DEVICE_PACKAGES := kmod-mt76x2
   SUPPORTED_DEVICES += mac1200rv2
 endef