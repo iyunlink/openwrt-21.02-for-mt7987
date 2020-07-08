iyunlink/openwrt-21.02-for-mt7987

commit 6db92b95dfe269d8efcf2e076ab8ab8294c969c3

kernel: add iscsi-initator support
Module is needed for using iscsi-initiator userspace applications

Signed-off-by: Lucian Cristian <lucian.cristian@gmail.com>
[added missing newline between kernel modules]
Signed-off-by: Petr Štetiar <ynezz@true.cz>

package/kernel/linux/modules/block.mk
@@ -270,6 +270,32 @@ endef
 $(eval $(call KernelPackage,dm-raid))
 
 
+define KernelPackage/iscsi-initiator
+  SUBMENU:=$(BLOCK_MENU)
+  TITLE:=iSCSI Initiator over TCP/IP
+  DEPENDS:=+kmod-scsi-core
+  KCONFIG:= \
+	CONFIG_INET \
+	CONFIG_SCSI_LOWLEVEL \
+	CONFIG_ISCSI_TCP \
+	CONFIG_SCSI_ISCSI_ATTRS
+  FILES:= \
+	$(LINUX_DIR)/drivers/scsi/iscsi_tcp.ko \
+	$(LINUX_DIR)/drivers/scsi/libiscsi.ko \
+	$(LINUX_DIR)/drivers/scsi/libiscsi_tcp.ko \
+	$(LINUX_DIR)/drivers/scsi/scsi_transport_iscsi.ko
+  AUTOLOAD:=$(call AutoProbe,libiscsi libiscsi_tcp scsi_transport_iscsi iscsi_tcp)
+endef
+
+define KernelPackage/iscsi-initiator/description
+The iSCSI Driver provides a host with the ability to access storage through an
+IP network. The driver uses the iSCSI protocol to transport SCSI requests and
+responses over a TCP/IP network between the host (the "initiator") and "targets".
+endef
+
+$(eval $(call KernelPackage,iscsi-initiator))
+
+
 define KernelPackage/md-mod
   SUBMENU:=$(BLOCK_MENU)
   TITLE:=MD RAID

commit f96887e1ca74ef8c62cd5277c644f96059c4ee2e

kernel: iscsi-initator: fix missing dependency
Fixes following issue:

 Package kmod-iscsi-initiator is missing dependencies for the following libraries:
 crypto_hash.ko

Fixes: 6db92b95dfe2 ("kernel: add iscsi-initator support")
Signed-off-by: Petr Štetiar <ynezz@true.cz>

package/kernel/linux/modules/block.mk
@@ -273,7 +273,7 @@ $(eval $(call KernelPackage,dm-raid))
 define KernelPackage/iscsi-initiator
   SUBMENU:=$(BLOCK_MENU)
   TITLE:=iSCSI Initiator over TCP/IP
-  DEPENDS:=+kmod-scsi-core
+  DEPENDS:=+kmod-scsi-core +kmod-crypto-hash
   KCONFIG:= \
 	CONFIG_INET \
 	CONFIG_SCSI_LOWLEVEL \