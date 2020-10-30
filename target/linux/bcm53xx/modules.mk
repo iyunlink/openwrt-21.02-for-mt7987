iyunlink/openwrt-21.02-for-mt7987

commit c7dcebda2c00ef8ec2af5714e49b4588b6dd490d

kernel: package bcm53xx i2c module
The BCM5301x SoCs do have i2c. Since this is only
being used by the Meraki MR32, this will be packaged
as a module.

Signed-off-by: Christian Lamparter <chunkeey@gmail.com>

target/linux/bcm53xx/modules.mk
@@ -35,3 +35,18 @@ define KernelPackage/phy-bcm-ns-usb3/description
 endef
 
 $(eval $(call KernelPackage,phy-bcm-ns-usb3))
+
+define KernelPackage/i2c-bcm-iproc
+  TITLE:=Broadcom iProc I2C controller
+  KCONFIG:=CONFIG_I2C_BCM_IPROC
+  DEPENDS:=@TARGET_bcm53xx +kmod-i2c-core
+  SUBMENU:=(I2C_MENU)
+  FILES:=$(LINUX_DIR)/drivers/i2c/busses/i2c-bcm-iproc.ko
+  AUTOLOAD:=$(call AutoLoad,59,i2c-bcm-iproc,1)
+endef
+
+define KernelPackage/i2c-bcm-iproc/description
+ Kernel module for the Broadcom iProc I2C controller.
+endef
+
+$(eval $(call KernelPackage,i2c-bcm-iproc))

commit e5072f55855aef421a7e230d3979e90877cf84c5

kernel: move kmod-i2c-bcm-iproc to I2C menu
A typo resulted in that module having it's own menu.
Move it into the I2C menu as it was most likely intended.

Fixes: c7dcebda2c ("kernel: package bcm53xx i2c module")
Signed-off-by: Daniel Golle <daniel@makrotopia.org>

target/linux/bcm53xx/modules.mk
@@ -40,7 +40,7 @@ define KernelPackage/i2c-bcm-iproc
   TITLE:=Broadcom iProc I2C controller
   KCONFIG:=CONFIG_I2C_BCM_IPROC
   DEPENDS:=@TARGET_bcm53xx +kmod-i2c-core
-  SUBMENU:=(I2C_MENU)
+  SUBMENU:=$(I2C_MENU)
   FILES:=$(LINUX_DIR)/drivers/i2c/busses/i2c-bcm-iproc.ko
   AUTOLOAD:=$(call AutoLoad,59,i2c-bcm-iproc,1)
 endef