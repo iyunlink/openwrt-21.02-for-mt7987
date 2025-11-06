ef6a6996cfcdb9ce00befd09396f422ef1157fb1
yuzui66 <yuzuihong@live.com>
2025-11-06 11:24:42 +0800

    update and include mtk-openwrt-feeds

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
-for file in `cat ./feeds/mtk_openwrt_feed/21.02/remove_list-mtwifi.txt`; do
-    rm -rf $file
-done
-
-cp -af ./feeds/mtk_openwrt_feed/21.02/files/* .
-cp -af ./feeds/mtk_openwrt_feed/tools .
-for file in $(find ./feeds/mtk_openwrt_feed/21.02/patches-base -name "*.patch" | sort); do patch -f -p1 -i ${file}; done
-for file in $(find ./feeds/mtk_openwrt_feed/21.02/patches-feeds -name "*.patch" | sort); do patch -f -p1 -i ${file}; done
+cp defaults.conf .config
+make defconfig
+make V=s -j1
+[ $? -ne 0 ] && {
+    echo "make failed"
+    exit 1
+}
+echo "make success"
+exit 0
